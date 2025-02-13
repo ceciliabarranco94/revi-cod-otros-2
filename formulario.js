4f460208dd46d04a51553b3addba773bf6fbc38a
Cecilia Barranco <cc94barranco@gmail.com>
2025-02-12 21:32:04 -0600

    add: primer intento 110225

--- formulario.js
+++ formulario.js
@@ -1,18 +1,32 @@
-var formulario = document.querySelector("#form")
+const formulario = document.querySelector("#form") //muchos declaraciones con var
+//cambiar todaas las var a let o const
 
-formulario.onsubmit = function(e) {
 
-  e.prevent();
+// esta no es la sintaxis para hacer un formulario
+// esta usando .onsubmit
+// en la documentación viene esta opción HTML # handler-onsubmit
+
+// en la documentación hay una función para hacer los formularios <form>: The Form element
+formulario.onsubmit = function(event) {  //esta linea da error en la consola 
+// Uncaught TypeError: Cannot set properties of null (setting 'onsubmit')
+//cambie e por event
+ 
+
+  event.prevent(); // la documentacion dice que esta es la sintaxis para prevent: preventDefault()
+
   
-  var n = formulario.elements[0]
-  var e = formulario.elements[1]
-  var na = formulario.elements[2]
+  let n = formulario.elements[0]
+  let e = formulario.elements[1] // esta linea da error en la consola
+  // a que se refiere con e
+  // Identifier 'e' has already been declared (at formulario.js:10:7)
 
-  var nombre = n.value
-  var edad = e.value
+  let na = formulario.elements[2]
 
-  var i = na.selectedIndex
-  var nacionalidad = na.options[i].value
+  let nombre = n.value
+  let edad = e.value
+
+  let i = na.selectedIndex
+  let nacionalidad = na.options[i].value
   console.log(nombre, edad)
   console.log(nacionalidad)
 
@@ -30,10 +44,10 @@ if (nombre.length > 0
   }
 }
 
-var botonBorrar = document.createElement("button")
+let botonBorrar = document.createElement("button")
 botonBorrar.textContent = "Eliminar invitado"
 botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
+let corteLinea = document.createElement("br")
 document.body.appendChild(corteLinea)
 document.body.appendChild(botonBorrar);
 
@@ -52,15 +66,15 @@ function agregarInvitado(nombre, edad, nacionalidad) {
     nacionalidad = "Peruana"
   }
 
-var lista = document.getElementById("lista-de-invitados")
+let lista = document.getElementById("lista-de-invitados")
 
-var elementoLista = document.createElement("div")
+let elementoLista = document.createElement("div")
 elementoLista.classList.added("elemento-lista")
 lista.appendChild(elementoLista)
 
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
+let spanNombre = document.createElement("span")
+let inputNombre = document.createElement("input")
+let espacio = document.createElement("br")
 spanNombre.textContent = "Nombre: "
 inputNombre.value = nombre 
 elementoLista.appendChild(spanNombre)
@@ -68,9 +82,9 @@ elementoLista.appendChild(inputNombre)
 elementoLista.appendChild(espacio)
 
 function crearElemento(descripcion, valor) {
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
+let spanNombre = document.createElement("span")
+let inputNombre = document.createElement("input")
+let espacio = document.createElement("br")
 spanNombre.textContent = descripcion + ": "
 inputNombre.value = valor 
 elementoLista.appendChild(spanNombre)
@@ -83,10 +97,10 @@ crearElemento("Edad", edad)
 crearElemento("Nacionalidad", nacionalidad)
 
 
-var botonBorrar = document.createElement("button")
+let botonBorrar = document.createElement("button")
 botonBorrar.textContent = "Eliminar invitado"
 botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
+let corteLinea = document.createElement("br")
 elementoLista.appendChild(corteLinea)
 elementoLista.appendChild(botonBorrar);
 
